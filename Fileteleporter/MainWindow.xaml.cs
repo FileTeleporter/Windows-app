62449ec9b5134ab0b60588b80f65eb13ced44909
Yohan Zbinden <yohan.zbinden@gmail.com>
2021-06-16 20:49:38 +0200

    fully working settings dialog and GPLv3 license

--- Fileteleporter/MainWindow.xaml.cs
+++ Fileteleporter/MainWindow.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2021  Jolan Aklin And Yohan Zbinden
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.IO;
@@ -22,19 +38,27 @@ namespace Fileteleporter
     /// </summary>
     public partial class MainWindow : Window
     {
-        private classes.Theme theme;
+        public classes.Data Data;
         public MainWindow()
         {
             bool whiteTheme = Properties.Settings.Default.whiteTheme;
             InitializeComponent();
-            this.theme = new classes.Theme(whiteTheme);
-            this.DataContext = this.theme;
+            this.Data = new classes.Data(whiteTheme);
+            this.DataContext = this.Data;
         }
 
         private void OpenSettings(object sender, MouseButtonEventArgs e)
         {
-            dialogs.Settings s = new dialogs.Settings(this.theme);
+            dialogs.Settings s = new dialogs.Settings(this);
             s.Show();
+        }
+        /// <summary>
+        /// called by the setting dialog when saving settings so it automatically update the theme
+        /// since the dialog directly edit the public property theme it does not required to pass an argument
+        /// </summary>
+        public void onSettingsUpdate()
+        {
+            this.DataContext = this.Data;
         }
     }
 }
--- Fileteleporter/dialogs/Settings.xaml.cs
+++ Fileteleporter/dialogs/Settings.xaml.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2021  Jolan Aklin And Yohan Zbinden
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,10 +35,35 @@ namespace Fileteleporter.dialogs
     /// </summary>
     public partial class Settings : Window
     {
-        public Settings(classes.Theme theme)
+        private MainWindow main;
+        public Settings(MainWindow caller)
         {
             InitializeComponent();
-            this.DataContext = theme;
+            this.main = caller;
+            this.DataContext = main.Data;
+            this.cbxWhiteTheme.IsChecked = Properties.Settings.Default.whiteTheme;
         }
+
+        private void Save(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                bool white = Convert.ToBoolean(this.cbxWhiteTheme.IsChecked);
+                main.Data = new classes.Data(white);
+                Properties.Settings.Default.whiteTheme = white;
+                Properties.Settings.Default.Save();
+                this.DataContext = main.Data;
+                main.onSettingsUpdate();                
+            }catch(Exception ex)
+            {
+                MessageBox.Show("An Error occured");
+            }
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {            
+            this.Close();
+        }
+
     }
 }
